5d62459d2e39e3fc82f2f80fae38be17d39d470f
livehass <silva.felipe12@hotmail.com>
2023-08-02 11:48:21 -0300

    novo array de itens repetidos

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -2,3 +2,19 @@ export default function ehUmCpf(campo){
     const cpf = campo.value.replace(/\.|-/g, "")
     console.log(cpf);
 }
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    return numerosRepetidos.includes(cpf)
+}
