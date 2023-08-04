11fd0fca86d96caa5052b2f465bd4b0970b57c78
livehass <silva.felipe12@hotmail.com>
2023-08-03 22:08:36 -0300

    nova função

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,11 @@
 export default function ehUmCpf(campo){
     const cpf = campo.value.replace(/\.|-/g, "")
-    validaNumerosRepetidos(cpf);
-    console.log(validaNumerosRepetidos(cpf));
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || ValidaSegundoDigito(cpf)) {
+        console.log("Esse Cpf não existe");
+    }else {
+        console.log("Esse Cpf Existe");
+    }
+    // console.log(validaNumerosRepetidos(cpf));
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -27,6 +31,34 @@ function validaPrimeiroDigito(cpf) {
     let multiplicador = 10;
 
     for( let tamanho = 0; tamanho < 9; tamanho++) {
-
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    
     }
+    
+    soma = (soma * 10) % 11;
+    
+    if(soma == 10 || soma == 11) {
+       soma = 0;
+    }
+    
+    return soma != cpf[9];
+    
+}
+function ValidaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let i = 0; i < 10; i ++) {
+        soma += cpf[i] * multiplicador;
+        multiplicador--;
+        
+    }
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    
+    return soma != cpf[9]
 }
