a659da8f366cf46fe2dd4775d86b3a5270e4cf7e
livehass <silva.felipe12@hotmail.com>
2023-08-07 20:32:01 -0300

    adiciona novo comentario

--- js/validaIdade.js
+++ js/validaIdade.js
@@ -9,5 +9,5 @@ function validaIdade(data) {
     const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
     
-    return dataAtual >= dataMais18;
+    return dataAtual >= dataMais18; // retorna true ou false para menor de idade
 }
