a09e0b7cd76cd7a2dc050d9ad143b710fae36151
livehass <silva.felipe12@hotmail.com>
2023-08-09 13:32:04 -0300

    commit de update

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ const mensagens = {
         tooShort: "Por favor, preencha um nome válido."
     },
     email: {
-        valueMissing: "O campo de e-mail não pode estar vazio.",
-        typeMismatch: "Por favor, preencha um email válido.",
-        tooShort: "Por favor, preencha um e-mail válido."
+        valueMissing: 'O campo de e-mail não pode estar vazio.',
+        typeMismatch: 'Por favor, preencha um email válido.',
+        tooShort: 'Por favor, preencha um e-mail válido.'
     },
     rg: {
         valueMissing: "O campo de RG não pode estar vazio.",
@@ -34,9 +34,9 @@ const mensagens = {
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
-        patternMismatch: "Por favor, preencha um CPF válido.",
-        customError: "O CPF digitado não existe.",
-        tooShort: "O campo de CPF não tem caractéres suficientes."
+        patternMismatch: 'Por favor, preencha um CPF válido.',
+        customError: 'O CPF digitado não existe.',
+        tooShort: 'O campo de CPF não tem caractéres suficientes.'
     },
     aniversario: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
@@ -48,6 +48,9 @@ const mensagens = {
 }
 
 function verificaCampo(campo){
+
+    let mensagem = "";
+
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCpf(campo);
     }
