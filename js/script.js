12e9a98615d3282e0f575584c8a79d94920843e7
livehass <silva.felipe12@hotmail.com>
2023-08-09 13:33:20 -0300

    commit de update

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ const tiposDeErro = [
 
 const mensagens = {
     nome: {
-        valueMissing: "O campo de nome não pode estar vazio.",
-        patternMismatch: "Por favor, preencha um nome válido.",
-        tooShort: "Por favor, preencha um nome válido."
+        valueMissing: 'O campo de nome não pode estar vazio.',
+        patternMismatch: 'Por favor, preencha um nome válido.',
+        tooShort: 'Por favor, preencha um nome válido.'
     },
     email: {
         valueMissing: 'O campo de e-mail não pode estar vazio.',
@@ -28,9 +28,9 @@ const mensagens = {
         tooShort: 'Por favor, preencha um e-mail válido.'
     },
     rg: {
-        valueMissing: "O campo de RG não pode estar vazio.",
-        patternMismatch: "Por favor, preencha um RG válido.",
-        tooShort: "O campo de RG não tem caractéres suficientes."
+        valueMissing: 'O campo de RG não pode estar vazio.',
+        patternMismatch: 'Por favor, preencha um RG válido.',
+        tooShort: 'O campo de RG não tem caractéres suficientes.'
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
