45f95d5cf5356bd1162d5c785b4cf9553548678c
livehass <silva.felipe12@hotmail.com>
2023-08-09 17:07:02 -0300

    change quotation marks

--- js/camera.js
+++ js/camera.js
@@ -1,1 +1,3 @@
-const botaoIniciarCamera = document.querySelector('[data-video-botao]');
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
+const campoCamera = document.querySelector("[data-camera]");
+const video = document.querySelector("[data-video]");
