2980089920fd8b6c9d97fa7952f471b08cf772c9
livehass <silva.felipe12@hotmail.com>
2023-08-09 17:11:30 -0300

    add get user media

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,9 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+
+
+botaoIniciarCamera.addEventListener("click", async function () {
+    const iniciarVideo = await navigator.mediaDevices
+    .getUserMedia({video: true, audio: false})
+} )
